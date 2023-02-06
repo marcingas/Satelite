3fd0a086be92ad6d6edbd94f29dd198004d70aac
Marcin <gasiorek.mail@gmail.com>
2023-02-06 14:05:24 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+//inFlight(new Jet());
+
+
+    }
+//    private static void inFlight(FlightEnabled flier){
+//        flier.takeOff();
+//        flier.fly();
+//        if(flier instanceof Trackable tracked){
+//            tracked.track();
+//        }
+//        flier.land();
+//    }
+
+}
---
+++ src/TestFly.java
@@ -0,0 +1,26 @@
+public class TestFly {
+    public static void main(String[] args) {
+        inFlight(new Jet());
+       orbit(new Satellite());
+
+    }
+
+
+
+
+
+    private static void inFlight(FlightEnabled flier){
+        flier.takeOff();
+        flier.transition(FlightStages.LAUNCH);
+        flier.fly();
+        if(flier instanceof Trackable tracked){
+            tracked.track();
+        }
+        flier.land();
+    }
+    private static void orbit(OrbitEarth flier){
+        flier.takeOff();
+        flier.fly();
+        flier.land();
+    }
+}
